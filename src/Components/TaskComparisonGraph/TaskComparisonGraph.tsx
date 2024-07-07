0e53a4aef7e27a6a9ddf2b28fd543b13940b739c
IRFAN UL HAQ <irfanulhaq228@gmail.com>
2024-07-07 11:51:39 +0500

    team analytics complete

--- src/Components/TaskComparisonGraph/TaskComparisonGraph.tsx
+++ src/Components/TaskComparisonGraph/TaskComparisonGraph.tsx
@@ -106,7 +106,7 @@ const TaskComparisonGraph = () => {
   };
 
   return (
-    <div className="shadow-sm mt-5 p-1 sm:p-3 rounded-[5px] scale-[0.95] sm:scale-100">
+    <div className="shadow-sm mt-5 p-1 sm:p-3 rounded-[5px] scale-[0.95] sm:scale-100 hidden sm:block">
       <Line data={data} options={options} />
     </div>
   );
